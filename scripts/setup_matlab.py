0a983b3a55a25dab9503909d55dbb68517d95c20
Johan Medrano <johan.medrano@ucl.ac.uk>
2024-11-26 19:24:05 +0000

    Update setup_matlab.py

--- scripts/setup_matlab.py
+++ scripts/setup_matlab.py
@@ -107,8 +107,8 @@ def download_matlab_runtime(system):
     dest_folder = tempfile.mkdtemp()
 
     zip_file_path = op.join(dest_folder, zip_name)
-    print(f"Downloading MATLAB Runtime from {runtime}...")
-    urllib.request.urlretrieve(runtime, zip_file_path)
+    print(f"Downloading MATLAB Runtime from {runtime_url}...")
+    urllib.request.urlretrieve(runtime_url, zip_file_path)
 
     print("Extracting MATLAB Runtime...")
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
