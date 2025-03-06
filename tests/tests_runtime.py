f51d4ad3f621876a0acfe7a3d7a740d0e703f23e
Johan Medrano <johmedr0@mit.edu>
2025-03-06 11:51:45 -0500

    [Enhancement] Add new tests for Runtime conversion

--- tests/tests_runtime.py
+++ tests/tests_runtime.py
@@ -42,6 +42,96 @@ class TestRuntime(unittest.TestCase):
 
         self.assertTrue(result is None)
 
+    def test_0d_empty_cell_to_matlab(self):
+        try: 
+            from spm import Runtime, Cell
+            idt = Runtime.call('eval', '@(x) x')
+            c = Cell()
+            c2 = idt(c)
+        except Exception:
+            self.fail('OD empty cell to Matlab failed.')
+
+        self.assertIsInstance(c2, Cell)
+        self.assertEqual(c2.shape, c.shape)
+
+    def test_1d_empty_cell_to_matlab(self):
+        try: 
+            from spm import Runtime, Cell
+            idt = Runtime.call('eval', '@(x) x')
+            c = Cell(3)
+            c2 = idt(c)
+        except Exception:
+            self.fail('1D empty cell to Matlab failed.')
+        
+        self.assertIsInstance(c2, Cell)
+        self.assertEqual(c2.shape, c.shape)
+
+    def test_2d_empty_cell_to_matlab(self):
+        try: 
+            from spm import Runtime, Cell
+            idt = Runtime.call('eval', '@(x) x')
+            c = Cell(3, 2)
+            c2 = idt(c)
+        except Exception:
+            self.fail('2D empty cell to Matlab failed.')
+
+        self.assertIsInstance(c2, Cell)
+        self.assertEqual(c2.shape, c.shape)
+
+    def test_1d_cell_to_matlab(self):
+        try:
+            from spm import Runtime, Cell
+            idt = Runtime.call('eval', '@(x) x')
+            c = Cell.from_any([1, 2, 3])
+            c2 = idt(c)
+        except Exception:
+            self.fail('1D cell to Matlab failed.')
+        
+        self.assertIsInstance(c2, Cell)
+        self.assertEqual(c2.shape, c.shape)
+        self.assertTrue(c2.tolist() == c.tolist())
+
+    def test_2d_cell_to_matlab(self):
+        try:
+            from spm import Runtime, Cell
+            import numpy as np
+            idt = Runtime.call('eval', '@(x) x')
+            c = Cell.from_array(np.random.randn(2, 3))
+            c2 = idt(c)
+        except Exception:
+            self.fail('1D cell to Matlab failed.')
+        
+        self.assertIsInstance(c2, Cell)
+        self.assertEqual(c2.shape, c.shape)
+        self.assertTrue(c2.tolist() == c.tolist())
+
+    def test_empty_struct_to_matlab(self):
+        try: 
+            from spm import Runtime, Struct
+            idt = Runtime.call('eval', '@(x) x')
+            s = Struct()
+            s2 = idt(s)
+        except Exception:
+            self.fail('Empty struct to Matlab failed.')
+        
+        self.assertIsInstance(s2, Struct)
+        self.assertEqual(s2.shape, s.shape)
+
+    def test_empty_array_to_matlab(self):
+        try: 
+            from spm import Runtime, Array
+            idt = Runtime.call('eval', '@(x) x')
+            a = Array()
+            a2 = idt(a)
+        except Exception:
+            self.fail('Empty array to Matlab failed.')
+
+        self.assertIsInstance(a2, Array)
+        self.assertEqual(a2.shape, a.shape)
+
+    
+
+
 
 if __name__ == '__main__':
     unittest.main()
