9d22146daa202793b4bb2a67433ea8f9155629a1
balbasty <yael.balbastre@gmail.com>
2025-06-19 15:33:06 +0100

    [ENH] mpython templates

---
+++ .mpython/templates/init.py
@@ -0,0 +1,12 @@
+from mpython import (
+    MatlabClass,
+    MatlabFunction,
+    Cell,
+    Struct,
+    Array,
+    SparseArray,
+)
+from ._runtime import Runtime
+from ._version import __version__
+
+<imports>
